78ccab3c7dfca3d84b565a171bf46862173a2cd8
Wiśniowski Mateusz <mateusz.wisniowski@wisniowski.pl>
2020-06-18 15:12:42 +0200

    cleanup

--- src/my_modules/MWTimePicker/TimerCilinder.tsx
+++ src/my_modules/MWTimePicker/TimerCilinder.tsx
@@ -17,7 +17,6 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-
 interface ITimerCilinder {
   spread: number
   maxValue: number
@@ -28,11 +27,10 @@ const TimerCilinder = (props: ITimerCilinder) => {
   const divRefs = new Array(8).fill(0).map(_ => React.createRef<HTMLDivElement>());
   const positions = new Array(8).fill(0).map((_, i) => new Vector2D(props.spread / 2, 0, i).rotate(-(Math.PI / 4 * i) + (Math.PI / 4 * 2)));
   const value = React.useRef(0);
-  // const positions = new Array(8).fill(0).map((_, i) => new Vector2D(props.spread / 2, 0, i).rotate(0));
+
 
   const getItemValue = (segment: number): string => {
     const localSeg = segment % 8;
-    // console.log({ localSeg }, value.current)
     switch (localSeg) {
       case 0: return `${value.current}`
       case 1: return value.current + 1 > props.maxValue ? '' : `${value.current + 1}`
@@ -44,34 +42,19 @@ const TimerCilinder = (props: ITimerCilinder) => {
   }
 
   const setCPosition = (rad: number) => {
+    // TODO need to add value snaping
     value.current = positions[0].rotatePrediction(rad, 8).segment;
-    // console.log('value', value.current)
     divRefs.forEach((ref, i) => {
       const pos = positions[i];
       const predPos = pos.rotatePrediction(rad, 8);
       const visibleSegments = [6, 7, 0, 1, 2];
       const sideSegments = [6, 2];
-      // console.log(predPos)
       if (ref.current) {
-        const seg = predPos.segment % 8;
         const opacity = clamp((predPos.y + 20) / props.spread, 0, 1);
         ref.current.style.opacity = `${opacity}`;
         ref.current.style.top = `${predPos.x + (props.spread / 2)}px`;
-        // console.log(ref.current.style.top, seg)
         ref.current.style.zIndex = `${Math.floor(predPos.y + props.spread / 2)}`;
-        if (visibleSegments.includes(seg)) {
-          // console.log({
-          //   ref: ref.current,
-          //   val: getItemValue(predPos.segment),
-          //   predPos,
-          //   index: ref.current.style.zIndex,
-          //   zi:pos.y + props.spread / 2,
-          //   opacity:ref.current.style.opacity,
-          //   opa:clamp((pos.y + 20) / props.spread, 0, 1)
-          // });
-          ref.current.innerHTML = getItemValue(predPos.segment)
-          // ref.current.innerHTML = '5';
-        }
+        ref.current.innerHTML = getItemValue(predPos.segment)
       }
     })
   }
